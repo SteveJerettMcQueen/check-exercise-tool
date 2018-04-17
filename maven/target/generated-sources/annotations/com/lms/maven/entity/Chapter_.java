43799f405fed4226e6a39eaa95fb905f7ff175d3
Steve McQueen <sm6668@stu.armstrong.edu>
2018-04-17 12:08:35 -0400

    modified, deleted files

--- maven/target/generated-sources/annotations/com/lms/maven/entity/Chapter_.java
+++ maven/target/generated-sources/annotations/com/lms/maven/entity/Chapter_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-16T13:22:04")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-17T11:30:41")
 @StaticMetamodel(Chapter.class)
 public class Chapter_ { 
 
